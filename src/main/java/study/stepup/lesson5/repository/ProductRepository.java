b340e7a11b4fe394466225df3ac6965449d44336
AAKudryavtsev <a.kudryavcev@dev.vtb.ru>
2024-03-09 22:08:30 +0300

    2

--- src/main/java/study/stepup/lesson5/repository/ProductRepository.java
+++ src/main/java/study/stepup/lesson5/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package study.stepup.lesson5.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import study.stepup.lesson5.model.Product;
 
